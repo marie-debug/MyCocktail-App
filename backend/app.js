8103dd0ee8098e95dcb932d1b6bed8bbf5db7244
Marion Akinyi <marion.akinyi@Marions-MacBook-Pro.local>
2023-01-24 17:59:29 +1000

    created route to get cocktail by alcohol type

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const app = express();
 //get cocktail by name
 app.get('/cocktails/:name', async function(req, res){
 
-    // console.log(req.params.name)
+    console.log(req.params.name)
     const response = await fetch('https://thecocktaildb.com/api/json/v1/1/search.php?s='+ req.params.name);
     const data = await response.json();
     let cocktailDetails = data.drinks
@@ -25,6 +25,41 @@ app.get('/cocktails/:name', async function(req, res){
     
    res.send(cocktails);
 });
+
+
+//get cocktail by alcohol
+app.get('/cocktails/alcohol/:name', async function(req, res){
+
+
+    // console.log(req.params.name)
+    const response = await fetch('https://thecocktaildb.com/api/json/v1/1/filter.php?i=' + req.params.name)
+    const data = await response.json();
+    let cocktailDetails = data.drinks
+    console.log(cocktailDetails)
+   
+    let cocktails = new Array()
+    cocktailDetails.forEach((item,index)=>{
+     
+        cocktails.push({
+                "name": item.strDrink,
+                "image": item.strDrinkThumb
+           
+            })
+    })
+   
+   
+   res.send(cocktails);
+ });
+ 
+
+
+
+
+
+
+
+
+
 
 
 
