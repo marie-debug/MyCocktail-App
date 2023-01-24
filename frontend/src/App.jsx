577b2af918e214913465c8eff56316435a14a1a2
Katrina Tomaszczyk <ktomaszczyk@hotmail.com>
2023-01-24 16:06:38 +1000

    updated changes to Search page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Search from './pages/Search'
+import Header from './components/Header'
+
 
 function App() {
   return (
     <>
       <Router>
-      <div>
+      <div className='container'>
+      <Header />
         <Routes>
           <Route path='/' element={<Search />} />
         </Routes>
