f5b39df640d7efcb3101a860731a3abf21271f71
Katrina Tomaszczyk <ktomaszczyk@hotmail.com>
2023-01-24 18:08:16 +1000

    added Header nav-bar

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,32 +1,35 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { FaWineBottle, FaCocktail } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
 
 function Search() {
+  const navigate = useNavigate()
+
+ 
+
   return (
     <>
-    <div className="App">
-      <div>
-        <section className='heading'>
-          <h1>1.2.3...Let's Party</h1>
-          <p>It's that easy to find your perfect cocktail.</p>
-          <FaCocktail /> 
-          <FaWineBottle />
-        </section>
-      </div>
-      <div>
-        <form>
-          <input 
-            type='search'
-            className='form-group'
-            placeholder='Search for your favourite cocktail or alcohol..'
-          />
-          <div className='form-group'>
-            <button type='submit' className='btn'>Search</button>
-          </div>
-        </form>
-      </div>
-    </div>
+      <section className='heading'>
+        <h1>1.2.3...Let's Party</h1>
+        <p>It's that easy to find your perfect cocktail.</p>
+        <FaCocktail /> 
+        <FaWineBottle />
+      </section>
+
+      <section className='content'>
+        <div classname='goals'>
+          <form>
+            <input 
+              type='search'
+              className='form-group'
+              placeholder='Search for your favourite cocktail or alcohol..'
+            />
+            <div className='form-group'>
+              <button type='submit' className='btn'>Search</button>
+            </div>
+          </form>
+        </div>
+      </section>
     </>
   )
 }
