7c8b623dd047e9c6bd9b0949b8d07e0b2a7a272b
Katrina Tomaszczyk <ktomaszczyk@hotmail.com>
2023-01-25 13:32:46 +1000

    footer added with styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Search from './pages/Search'
 import Results from './pages/Results'
+import Footer from './components/Footer'
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
             <Route path='/' element={<Search />} />
             <Route path='/results' element={<Results />} />
           </Routes>
+          <Footer />
         </div>
       </Router>
     </>
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import { FaCopyright } from "react-icons/fa"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer-p">
+      <footer class="footer mt-auto py-7 bg-light">
+        <div class="container-footer">
+          <span class="text-muted">My Cocktail | <FaCopyright /> Copyright | 2023</span>
+        </div>
+      </footer>
+    </div>
   )
 }
 
