db06e251ef1ac01dd9a2036cb07822b40f3491ed
Katrina Tomaszczyk <ktomaszczyk@hotmail.com>
2023-01-25 12:33:27 +1000

    updated offcanvas header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,21 +5,26 @@ const Header = () => {
 
   return (
     <header className='header'>
-      <nav class="navbar">
+      <nav class="navbar" aria-label="Light offcanvas navbar">
         <div class="container-fluid">
           <div class="navbar-header">
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/results">Results</Link>
-              </li>
-            </ul>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbarLight" aria-controls="offcanvasNavbarLight">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbarLight" aria-labelledby="offcanvasNavbarLightLabel">
+            <div class="offcanvas-header">
+              <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/results">Results</Link>
+                </li>
+              </ul>
+            </div>
           </div>
             <Link class="navbar-brand" to='/' >My Cocktail</Link>
           </div>
