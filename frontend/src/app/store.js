83d551b14410538f6decea0b4dc85d95ffd7b99e
Katrina Tomaszczyk <ktomaszczyk@hotmail.com>
2023-01-29 13:58:06 +1000

    seperated add cocktail into a component

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import cocktailReducer from '../features/cocktails/cocktailSlice.'
 
 export const store = configureStore({
   reducer: {
+    cocktails: cocktailReducer
   },
 });
